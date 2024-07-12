f019cc4299713dfbc42bd8b524c7c780b0b787c4
lael12 <laelaytemesgenet12@gmail.com>
2024-07-12 04:27:02 +0300

    Efficiency = 0

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let userInput
+let u=prompt("rock paper scissor?")
+function getHumanChoice(){
+    if(u.toLowerCase()== 'rock'){
+        userInput = 0; 
+    }
+    else if(u.toLowerCase()== 'paper'){
+        userInput = 1; 
+    }
+    else if(u.toLowerCase()== 'scissor'){
+        userInput = 2; 
+    }
+    else {
+        console.log("Invalid Input!!!")
+    }
+}
+let computer = Math.floor(Math.random() * 3)
+console.log(computer)
+if (userInput == computer){
+    console.log("Draw")
+}
+else if(userInput+1 == computer || computer+2 ==userInput){
+    console.log("Computer Won")
+}
+else{
+    console.log("You Won")
+}
